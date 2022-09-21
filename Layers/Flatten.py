65f5c37d8c3160bab98c449f46796a37335c7383
Tim Roerup <timroerup@gmail.com>
2022-09-21 15:04:44 +0200

    Documented Flatten

--- Layers/Flatten.py
+++ Layers/Flatten.py
@@ -3,14 +3,26 @@ from Layers import Base
 
 
 class Flatten(Base.BaseLayer):
+    """Flatten layer that flattens the input tensor.
+    """
     def __init__(self):
         super().__init__()
         self.shape_in = None
 
     def forward(self, input_tensor):
+        """Flattens each multidimensional sample from the batch of the input tensor.
+
+        :param input_tensor: Input tensor to be flattened.
+        :return: Flattened tensor consisting of 1D batch-samples.
+        """
         self.shape_in = np.shape(input_tensor)
         batch_size = self.shape_in[0]
         return input_tensor.reshape(batch_size, -1)
 
     def backward(self, error_tensor):
+        """Takes a flattened tensor and reshapes it to its original shape.
+
+        :param error_tensor: Flattened gradient tensor from upper layer.
+        :return: Original shaped gradient tensor.
+        """
         return error_tensor.reshape(self.shape_in)
